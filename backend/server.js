4a26ed8ab14ee2f82b00f8ee6e3d22eb766ff3ee
Avi0304 <avip56325@gmail.com>
2025-02-10 12:09:24 +0530

    fix frontend link bug

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://event-mangement-system.vercel.app/",
+        origin: "https://event-mangement-system.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
