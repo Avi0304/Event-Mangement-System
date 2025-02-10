1f4678f2478da13034630d4e9cfc9d45a5d5cb27
Avi0304 <avip56325@gmail.com>
2025-02-10 12:05:54 +0530

    add frontend link

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://event-mangement-system.vercel.app/",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
